c18f40056558fa99865e7f2ba159e771a3694836
Zandroid505 <zakariaantifit@hotmail.com>
2021-10-16 10:26:00 -0400

    Finished exercise44, still need to create tests and uml diagram

--- exercise44/src/main/java/baseline/Product.java
+++ exercise44/src/main/java/baseline/Product.java
@@ -1,63 +1,100 @@
 package baseline;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Product {
     private static final Scanner input = new Scanner(System.in);
-    private String name;
+    private final String name;
     private double price;
     private int quantity;
 
     public Product(String name, double price, int quantity) {
         //Initialize fields to constructor arguments
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
 
-    public List<Product> convertJsonToProduct(String jsonText) {
-        //Create jsonParser
-        //nameTemp
-        //priceTemp
-        //quantityTemp
-        //ArrayList inventory
+    public List<Product> convertJsonToProduct(String jsonFilePath) {
+        List<Product> inventory = new ArrayList<>();
 
         //try
+        try {
             //Create file object with json file path
+            FileReader inFile = new FileReader(jsonFilePath);
+
             //obj = jsonParser(file object)
-            //productJsonObj = obj
+            JsonObject obj = (JsonObject)JsonParser.parseReader(inFile);
 
             //iterator = productJsonObj.get("products")
+            JsonArray iterator = obj.getAsJsonArray("products");
 
             //for(amount of products in json file)
+            for(int i = 0; i < iterator.size(); i++) {
                 //products = iterator.get(i)
-
-                //nameTemp = name of product
-                //priceTemp = price of product
-                //quantityTemp = quantity of product
+                JsonObject product = iterator.get(i).getAsJsonObject();
 
                 //productTemp = new Product(nameTemp, priceTemp, quantityTemp)
+                Product productTemp = new Product(product.get("name").getAsString(),
+                        product.get("price").getAsDouble(), product.get("quantity").getAsInt());
+
                 //inventory.add(productTemp)
-        //catch(FileNotFoundException | IOException | ParseException | Exception e)
+                inventory.add(productTemp);
+            }
+        }//catch(FileNotFoundException | IOException | ParseException | Exception e)
+        catch(Exception e) {
             //printStackTrace
+            e.printStackTrace();
+        }
 
         //return inventory
+        return inventory;
     }
 
     public void searchForProduct(List<Product> inventory) {
-        //itemFound = false
+        boolean itemFound = false;
 
         //while(!itemFound)
+        while(!itemFound) {
             //print "What is the product name? "
+            System.out.print("What is the product name? ");
+
             //productQuery = input
-            //if(productQuery is in inventory)
-                //itemFound = true
-                //printItemInfo(product)
-            //else
+            String productQuery = input.nextLine();
+
+            for(Product p: inventory) {
+                //if(productQuery is in inventory)
+                if (p.name.equals(productQuery)) {
+                    //itemFound = true
+                    itemFound = true;
+
+                    //printItemInfo(product)
+                    printItemInfo(p);
+                }
+            }
+            if(!itemFound) {
                 //print "Sorry, that product was not found in our inventory."
+                System.out.println("Sorry, that product was not found in our inventory.");
+            }
+
+        }
     }
 
-    private void printItemInfo(Product item) {
-        //print "Name: item.name"
-        //print "Price: item.price"
-        //print "Quantity: item.quantity"
+    private void printItemInfo(Product product) {
+        //print "Name: product.name"
+        System.out.printf("Name: %s%n", product.name);
+
+        //print "Price: product.price"
+        System.out.printf("Price: %.2f%n", product.price);
+
+        //print "Quantity: product.quantity"
+        System.out.printf("Quantity: %d%n", product.quantity);
     }
 }
--- exercise44/src/main/java/baseline/Solution44.java
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -6,14 +6,21 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Solution44 {
     public static void main(String[] args) {
+        Product manager = new Product("", 0.0, 0);
+
         //inventory (Array of Products)
+        List<Product> inventory;
 
         //inventory = convertJsonToProduct
+        inventory = manager.convertJsonToProduct("data/exercise44_input.json");
+
         //searchForProduct
-        
+        manager.searchForProduct(inventory);
+
     }
 
 }
